dd9451f5d5279672676de0090374defbe085f47d
chnishank <chnishank@gmail.com>
2021-08-20 13:17:28 +0530

    Project0

--- P0.py
+++ P0.py
@@ -6,47 +6,36 @@ import datetime
 import re
 from QueryPrint import Qprint
 
+""" Making a Connection with MongoClient """
+client=MongoClient()    # This will connect on the default host and port
 
-client=MongoClient()
+""" Getting a Database """
+db=client['P0']          
 
-db=client['P0']          #db=client.P0
-
-
-
-
-#x=db.list_collection_names()
-#c=x[0]
-#d=db.get_collection(c)
-
-d=db['restaurants']        #d=db.restaurants
+""" Getting a Collection """
+d=db['restaurants']       
 
 print("Enter 1 to execute User Query","Enter 2 to execute Queries in P0 Exercise",sep='\n')
 choice=int(input("Please Enter choice: "))
 
-if choice==1:
-    flag=True
-    while flag:
-        query=input('\nEnter query:\n')
-        temp=input('\nPress  Enter to Continue....\n')
+flag=True
+while flag:
 
-        for e in eval(query):
-            pprint(e)
-        s=int(input("\nEnter 1 to continue...  or 2 to stop: "))
-        if s==2:
-            flag=False
-else:
-    flag=True
-    while flag:
+    if choice==1:
+    
+        query=input('\nEnter query:\n')
+
+    else:
         i=int(input("Please Enter Query Number: " ))
 
         print("\n",Qprint(i))
     
         query= queries[i]
         print('The Query to be Executed is:',query,sep='\n')
-        temp=input('\nPress  Enter to Continue....\n')
+    temp=input('\nPress  Enter to Continue....\n')
 
-        for e in eval(query):
-            pprint(e)
-        s=int(input("\nEnter 1 to continue...  or 2 to stop: "))
-        if s==2:
-            flag=False
+    for e in eval(query):
+        pprint(e)
+    s=int(input("\nEnter 1 to continue...  or 2 to stop: "))
+    if s==2:
+        flag=False
